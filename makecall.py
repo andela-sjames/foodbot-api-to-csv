415c9cd693ff302973b7c3a37c061f0032a4f21b
Samuel James <samuel.james@andela.com>
2015-12-12 20:48:20 +0100

    call foodbot api rating api

---
+++ makecall.py
@@ -0,0 +1,24 @@
+import requests
+
+sample = requests.get('http://slack-foodbot.herokuapp.com/api/rating/',)
+
+api_result = sample.json()
+headers = 'DATE, RATE, COMMENT, DAY, FOOD, MEAL\n'
+
+
+def readfile():
+    return_string = []
+    resp = ''
+    for results in api_result['results']:
+        return_string = [str(results['date']), str(results['rate']),
+                         str(results['comment']), str(results['menu']['day']),
+                         str(results['menu']['food']),
+                         str(results['menu']['meal'])]
+
+        resp += ', '.join(return_string) + '\n'
+    return resp
+
+f = open('food.csv', 'w')
+f.write(headers)
+f.write(readfile())
+f.close()
